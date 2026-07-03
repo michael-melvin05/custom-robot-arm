0d2bce530e87a4327dda5fb3d96c2dc7928c0e69
Michael Melvin <mmelvin5742@sdsu.edu>
2026-07-03 13:46:12 -0800

    Update Stepper-2-Code.ino

--- Versions/V1/Code/Stepper-2-Code.ino
+++ Versions/V1/Code/Stepper-2-Code.ino
@@ -1,1 +1,30 @@
+#include <Stepper.h>
 
+const int stepsPerRevolution = 2048;
+
+
+Stepper stepperMotor(stepsPerRevolution, 4, 3, 5, 6);
+
+
+const int cwButton = 5;
+const int ccwButton = 6;
+
+void setup() {
+  stepperMotor.setSpeed(16);
+
+  pinMode(cwButton, INPUT_PULLUP);
+  pinMode(ccwButton, INPUT_PULLUP);
+
+  Serial.begin(9600);
+  Serial.println("Motor 2 Ready");
+}
+
+void loop() {
+  if (digitalRead(cwButton) == LOW) {
+    stepperMotor.step(1);
+  }
+
+  if (digitalRead(ccwButton) == LOW) {
+    stepperMotor.step(-1);
+  }
+}
